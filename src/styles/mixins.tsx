c6d7b474f7bbea23b5dad1cd457aa6ab287104ab
Ramon Guillermo <ramon.alvarez@dspot.com.pl>
2024-09-29 14:58:43 +0300

    fix: style prop

--- src/styles/mixins.tsx
+++ src/styles/mixins.tsx
@@ -74,11 +74,7 @@ export const titleLarge = css`
 
 export const elevation1 = css`
   /* Elevation 1 */
-  box-shadow:
-    0px 0px 0px 1px rgba(0, 0, 0, 0.05),
-    0px 1px 1px -0.5px rgba(0, 0, 0, 0.05),
-    0px 3px 3px -1.5px rgba(0, 0, 0, 0.04),
-    0px 6px 6px -3px rgba(0, 0, 0, 0.04),
-    0px 12px 12px -6px rgba(0, 0, 0, 0.04),
-    0px 24px 24px -12px rgba(0, 0, 0, 0.04);
+  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.05), 0px 1px 1px -0.5px rgba(0, 0, 0, 0.05),
+    0px 3px 3px -1.5px rgba(0, 0, 0, 0.04), 0px 6px 6px -3px rgba(0, 0, 0, 0.04),
+    0px 12px 12px -6px rgba(0, 0, 0, 0.04), 0px 24px 24px -12px rgba(0, 0, 0, 0.04);
 `;
